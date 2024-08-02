e81afb2dcb3d0b5fa605358eee2e91a0c33929f4
koacaiia <koacaiia@gmail.com>
2024-08-02 15:50:06 +0900

    0802_file

--- main.js
+++ main.js
@@ -71,15 +71,17 @@ function getData(date){
             document.querySelector("#tBodyIn").appendChild(tr);
             tr.addEventListener("click",(e)=>{
                 const trList = document.querySelectorAll("#tBodyIn tr");
-                trList.forEach((e)=>{
-                    e.classList.remove("clicked");
-                });
                 e.target.parentNode.classList.toggle("clicked");
+                trList.forEach((e)=>{
+                    if(!e.classList.contains("clicked")){
+                         e.classList.remove("clicked");}
+                });
                 document.querySelector("#mainOut").style="display:none";
                 popUp(tr.id,"InCargo");
             });
+            if(val[i]["workStatus"]!="1"){
+                tr.style="color:red;";}
         }
-        
     }).
     catch((e)=>{
         alert(e);
@@ -126,18 +128,141 @@ getData(titleDate.innerHTML);
 function popUp(ref,ioValue){
     const pop = document.querySelector("#mainPop");
     pop.style="display:block";
-    database_f.ref(ref).get().then((snapshot)=>{
-        const val = snapshot.val();
-        const container = val["container"];
-        console.log(container,val);
-        database_f.ref(ref).parent.get().then((snapshot)=>{
-            const val = snapshot.val();
-            console.log(val);
-        }).catch((e)=>{
-            console.log(e)});
-
-    }).catch((e)=>{});
+    const mainDiv = document.querySelector("#mainPopDiv");
+    mainDiv.replaceChildren();
     
+
+    const table= document.createElement("table");
+    const thead = document.createElement("thead");
+    const tr = document.createElement("tr");
+    const thList=["관리번호","품명","PLT","EA","비고"];
+    thList.forEach((e)=>{
+        const th = document.createElement("th");
+        th.innerHTML=e;
+        tr.appendChild(th);
+    });
+    thead.appendChild(tr);
+    table.appendChild(thead);
+    mainDiv.appendChild(table);
+    const fileDiv = document.createElement("div");
+    const fileInput = document.createElement("input");
+    fileInput.type="file";
+    fileInput.id="fileInput";
+    fileInput.multiple="multiple";
+
+    const resizeImage = (settings) => {
+        const file = settings.file;
+        const maxSize = settings.maxSize;
+        const reader = new FileReader();
+        const image = new Image();
+        const canvas = document.createElement("canvas");
+      
+        const dataURItoBlob = (dataURI) => {
+          const bytes =
+            dataURI.split(",")[0].indexOf("base64") >= 0
+              ? atob(dataURI.split(",")[1])
+              : unescape(dataURI.split(",")[1]);
+          const mime = dataURI.split(",")[0].split(":")[1].split(";")[0];
+          const max = bytes.length;
+          const ia = new Uint8Array(max);
+          for (let i = 0; i < max; i++) ia[i] = bytes.charCodeAt(i);
+          return new Blob([ia], { type: mime });
+        };
+      
+        const resize = () => {
+          let width = image.width;
+          let height = image.height;
+          if (width > height) {
+            if (width > maxSize) {
+              height *= maxSize / width;
+              width = maxSize;
+            }
+          } else {
+            if (height > maxSize) {
+              width *= maxSize / height;
+              height = maxSize;
+            }
+          }
+          canvas.width = width;
+          canvas.height = height;
+          canvas.getContext("2d").drawImage(image, 0, 0, width, height);
+          const dataUrl = canvas.toDataURL("image/jpeg");
+          return dataURItoBlob(dataUrl);
+        };
+      
+        return new Promise((ok, no) => {
+          if (!file) {
+            return;
+          }
+          if (!file.type.match(/image.*/)) {
+            no(new Error("Not an image"));
+            return;
+          }
+          reader.onload = (readerEvent) => {
+            image.onload = () => {
+              return ok(resize());
+            };
+            image.src = readerEvent.target.result;
+          };
+          reader.readAsDataURL(file);
+        });
+      };
+    
+      const handleImgInput = (e) => {
+        upfileList = e.target.files;
+        for(let i=0;i<e.target.files.length;i++){
+        console.log(e.target.files[i]);     
+        const config = {
+          file: e.target.files[i],
+          maxSize: 150,
+        };
+        const imgTag = document.createElement("td");
+        resizeImage(config)
+          .then((resizedImage) => {
+            const url = window.URL.createObjectURL(resizedImage);
+            const img = document.createElement("img");
+            img.setAttribute("src", url);
+            img.className = "profile-img";
+            img.style.display = "block";
+            imgTag.appendChild(img);
+            fileTr.appendChild(imgTag);
+          })
+          .then(() => {
+            const img = document.querySelector(".profile-img");
+            img.onload = () => {
+              const widthDiff = (img.clientWidth - imgTag.offsetWidth) / 2;
+              const heightDiff = (img.clientHeight - imgTag.offsetHeight) / 2;
+              img.style.transform = `translate( -${widthDiff}px , -${heightDiff}px)`;
+            };
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+        }
+      };
+
+    fileInput.addEventListener("change",handleImgInput);
+    const fileTable = document.createElement("table");
+    const fileTr = document.createElement("tr");
+    fileTable.appendChild(fileTr);
+    fileDiv.appendChild(fileInput);
+    fileDiv.appendChild(fileTable);
+    fileDiv.style="display:grid;grid-template-rows:1fr 8fr";
+    mainDiv.appendChild(fileDiv);
+    mainDiv.style="display:grid;grid-template-rows:1fr 1fr";
+    if(ioValue=="InCargo"){
+        database_f.ref(ref).get().then((snapshot)=>{
+            const val = snapshot.val();
+            const container = val["container"];
+            console.log(container,val);
+            database_f.ref(ref).parent.get().then((snapshot)=>{
+                const val = snapshot.val();
+                console.log(val);
+            }).catch((e)=>{
+                console.log(e)});
+    
+        }).catch((e)=>{});
+    }
 };
 function popClose(){
     document.querySelector("#mainPop").style="display:none";
