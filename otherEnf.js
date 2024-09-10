6fdb5889d68c547206c638d06f679ae90b71b4fd
koacaiia <koacaiia@gmail.com>
2024-09-10 17:39:41 +0900

    0910

--- otherEnf.js
+++ otherEnf.js
@@ -14,8 +14,9 @@ const messaging = firebase.messaging();
 const storage_f = firebase.storage();
 const deptName = "WareHouseDept2";
 const pop = document.querySelector('#contentsEnf');
+const inputContents = document.querySelector('#inputContents');
 const eNfTableTbody = document.querySelector("#eNfTableTbody");
-const inputList = pop.querySelectorAll('input');
+const inputList = inputContents.querySelectorAll('input');
 
 function getEnfData(){
     eNfTableTbody.replaceChildren();
@@ -33,13 +34,14 @@ function getEnfData(){
             enfRow.id=enfValue[key].keyValue;
             eNfTableTbody.appendChild(enfRow);
             enfRow.addEventListener('click',(e)=>{
-                removeSelected();
+                // removeSelected();
                 enfRow.classList.toggle('file-selected');
                 eNfReg(enfValue[key]);
 
             });
         }
     }).catch((error)=>{console.log(error)});
+    // getImgData();
 
 }
 getEnfData();
@@ -49,8 +51,10 @@ function eNfReg(enfValue){
     const keyValue=["name","checkContent","checkAskDate","checkDate","purchaseOrderCreationDate","purchaseOrderContent","purchaseOrderAmount","purchaseOrderPaymentDate","actionDate","realAmount","remark"];
     pop.classList.toggle('popUp');
     if(enfValue!=undefined){
-        inputList.forEach((input,index)=>{input.value=enfValue[keyValue[index]]});
+        inputList.forEach((input,index)=>{
+          input.value=enfValue[keyValue[index]]});
     }
+    getImgData();   
 }
 function enfSubmit(){
  const baseRef="DeptName/"+deptName+"/EquipNFacility";
@@ -76,3 +80,124 @@ function enfClose(){
     pop.classList.toggle('popUp');
     removeSelected();
 }
+function getImgData(){
+    const imgRef = storage_f.ref("DeptName/"+deptName+"/EquipNFacility/"+eNfTableTbody.querySelectorAll('.file-selected')[0].id);
+    imgRef.listAll().then((res)=>{
+        res.items.forEach((itemRef)=>{
+            itemRef.getDownloadURL().then((url)=>{
+                const imgName = itemRef.name.split("_");
+                const imgTag = document.querySelector(`#${imgName[1]}`);
+                imgTag.src=url;
+            });
+        });
+    });
+}
+function imgUpLoad(e){
+    const imgRef = eNfTableTbody.querySelectorAll('.file-selected')[0].id;
+    let putUpRef;
+    if(e.target.id=="imgBbtn"){
+        putUpRef ="Before";}
+    else if(e.target.id=="imgAbtn"){
+        putUpRef ="After";
+    }
+    const upRef = storage_f.ref("DeptName/"+deptName+"/EquipNFacility/"+imgRef+"/"+putUpRef);
+    upRef.put(upfileList[0]).then(()=>{
+        alert("이미지 업로드 완료");
+        getImgData();
+    }).catch((error)=>{console.log(error)});
+}
+
+const resizeImage = (settings) => {
+    const file = settings.file;
+    const maxSize = settings.maxSize;
+    const reader = new FileReader();
+    const image = new Image();
+    const canvas = document.createElement("canvas");
+  
+    const dataURItoBlob = (dataURI) => {
+      const bytes =
+        dataURI.split(",")[0].indexOf("base64") >= 0
+          ? atob(dataURI.split(",")[1])
+          : unescape(dataURI.split(",")[1]);
+      const mime = dataURI.split(",")[0].split(":")[1].split(";")[0];
+      const max = bytes.length;
+      const ia = new Uint8Array(max);
+      for (let i = 0; i < max; i++) ia[i] = bytes.charCodeAt(i);
+      return new Blob([ia], { type: mime });
+    };
+  
+    const resize = () => {
+      let width = image.width;
+      let height = image.height;
+      if (width > height) {
+        if (width > maxSize) {
+          height *= maxSize / width;
+          width = maxSize;
+        }
+      } else {
+        if (height > maxSize) {
+          width *= maxSize / height;
+          height = maxSize;
+        }
+      }
+      canvas.width = width;
+      canvas.height = height;
+      canvas.getContext("2d").drawImage(image, 0, 0, width, height);
+      const dataUrl = canvas.toDataURL("image/jpeg");
+      return dataURItoBlob(dataUrl);
+    };
+  
+    return new Promise((ok, no) => {
+      if (!file) {
+        return;
+      }
+      if (!file.type.match(/image.*/)) {
+        no(new Error("Not an image"));
+        return;
+      }
+      reader.onload = (readerEvent) => {
+        image.onload = () => {
+          return ok(resize());
+        };
+        image.src = readerEvent.target.result;
+      };
+      reader.readAsDataURL(file);
+    });
+  };
+
+  const handleImgInput = (e) => {
+    const fileTr = document.querySelector("#imgBeforeTr");
+    fileTr.replaceChildren();
+    upfileList = e.target.files;
+    for(let i=0;i<e.target.files.length;i++){
+    const config = {
+      file: e.target.files[i],
+      maxSize: 1500,
+    };
+    const imgTag = document.createElement("td");
+    resizeImage(config)
+      .then((resizedImage) => {
+        const url = window.URL.createObjectURL(resizedImage);
+        const img = document.createElement("img");
+        img.className = "profile-img";
+        img.addEventListener("click", (e) => {
+          img.parentNode.classList.toggle("file-selected");
+        });
+        img.setAttribute("src", url);
+        img.style.display = "block";
+        img.style.width="100%";
+        img.style.height="22vh";
+        imgTag.appendChild(img);
+        fileTr.appendChild(imgTag);
+      })
+      .then(() => {
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    }
+  };
+
+  document.querySelectorAll(".fileInput input").forEach((input)=>{
+    input.addEventListener("change",handleImgInput);
+  });
