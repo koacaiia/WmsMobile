6d157446a7a010ae38d22efb6113389b8264f799
koacaiia <koacaiia@gmail.com>
2025-02-19 17:23:04 +0900

    0219

--- main.js
+++ main.js
@@ -660,7 +660,7 @@ function osSubmit(){
   }).catch((e)=>{});
 }
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/https://github.com/koacaiia/WmsMobile/blob/main/firebase-messaging-sw.js')
+  navigator.serviceWorker.register('/blob/main/firebase-messaging-sw.js')
     .then((registration) => {
       console.log('Service Worker registered with scope:', registration.scope);
     })
